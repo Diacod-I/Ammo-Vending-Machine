c540f6fd592ac85b17b26e62841c4b24cf638690
Luci <98768417+Diacod-I@users.noreply.github.com>
2022-04-24 18:29:14 +0530

    Add files via upload
    Use an IDE or Online Python Interpreter to run this Program.

---
+++ AmmoVendingMachine.py
@@ -0,0 +1,29 @@
+print("Welcome, soldier..\n\nThis is an ammuntion vending machine..🔫🔫 \n\nPress 1 for Assault Rifle ammo\nPress 2 for SMG ammo\nPress 3 for LMG ammo\nPress 4 for Shotgun ammo\nPress 5 for Sniper Rifle ammo\nPress 6 for Pistol ammo\n\nPress 0 to leave\n")
+i = int(input("Enter choice of ammo: "))
+while i>=0:
+        def ammo_type(i):
+                switcher={
+                        1:'Assault Rifle Ammo',
+                        2:'SMG Ammo',
+                        3:'LMG Ammo',
+                        4:'Shotgun Ammo',
+                        5:'Sniper Rifle Ammo',
+                        6:'Pistol Ammo'
+                        }
+                        
+                return switcher.get(i)
+
+        if i>=1 and i<=6:
+                print("You chose",ammo_type(i))
+                print("Now enter the number of ammo needed")
+                x = int(input())
+                print("\n")
+                print("✅✅Order validated✅✅\n",x,"rounds of",ammo_type(i),"have been obtained..")
+                print("\nThank you..Have a nice day👋👋")
+                break
+        elif i==0:
+                print("\nHope we can be of assistance in the future..🙁🙁\n\nThank you..Have a nice day👋👋")
+                break
+        else: 
+                print("❌❌Invalid❌❌\n\nTry again...😵😵")
+                break
